b1e0b6b8ad85556de76a87e11136993d798b3f1a
Lala <lalagg@hotmail.com>
2020-06-20 09:59:13 -0400

    SCHEMA, new files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burguersDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE burgers
+(
+ id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ burger_name VARCHAR(255) NOT NULL,
+ devoured BOOL DEFAULT false,
+ createdAt TIMESTAMP NOT NULL,
+);
