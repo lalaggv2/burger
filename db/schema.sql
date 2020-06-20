133a401e434b9f9592585595e0a25a0f1ab5d0c5
Lala <lalagg@hotmail.com>
2020-06-20 10:10:31 -0400

    timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,5 @@ CREATE TABLE burgers
  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
  burger_name VARCHAR(255) NOT NULL,
  devoured BOOL DEFAULT false,
- createdAt TIMESTAMP NOT NULL,
+ createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp,
 );
